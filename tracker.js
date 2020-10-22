0611f6dcf27ce615f3c46bb41d88981b3a991d00
Supipi Silva <nirmanialgama@gmail.com>
2020-10-22 23:16:13 +0200

    fixed clear input and remove habits

--- tracker.js
+++ tracker.js
@@ -1,10 +1,11 @@
 let nextTaskId = 1;
 const list = document.getElementById("tasks-container");
+let tasks = [];
 function addHabit(habit, id) {
     const text = `<div class="task-holder">
         <p class="text">${habit}
-        <i class="fa fa-trash-alt toright" job="delete" id="${id}"></i>
         </p>
+        <button onclick="removeHabit(this)" type="button" class="deletebtn"><i class="fa fa-trash-alt" job="delete"></i></button>
     </div>
     <div class="checkbox-holder">
     <div class="task-checkbox"><i class="far fa-square center"></i></div>
@@ -17,24 +18,37 @@ function addHabit(habit, id) {
 </div>`
     const position = "beforeEnd"
     list.insertAdjacentHTML(position, text);
-    container.push({
+    tasks.push({
         name: habit,
         id: id
     })
 }
+
+
 
 const input = document.getElementById("input-habit");
 document.addEventListener("keyup", function (e) {
     if (e.keyCode == 13) {
         addNewHabit();
     }
-
 })
 
 function addNewHabit() {
     const habit = input.value;
     if (habit) {
         addHabit(habit, nextTaskId++);
+        clearTextBox();
     }
     input.value = "";
 }
+
+function removeHabit(element) {
+    element.parentNode.nextElementSibling.remove();
+    element.parentNode.remove();
+}
+
+function clearTextBox() {
+    document.getElementById("input-habit").value = "";
+}
+
+
