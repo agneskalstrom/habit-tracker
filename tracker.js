5f41e67b96b370c1cc03bcd23a474769fd290936
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2020-10-23 20:56:10 +0200

    Updates for header + fixed collapsing list

--- tracker.js
+++ tracker.js
@@ -1,20 +1,44 @@
+const userName = prompt("Welcome! Please enter your name:");
+
+const date = new Date();
+const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+const timeOptions = { hour: '2-digit', minute:'2-digit' };
+document.getElementById("date").innerText = date.toLocaleDateString('en-US', dateOptions);
+document.getElementById("time").innerText = date.toLocaleTimeString('en-US', timeOptions);
+
+let greeting;
+let currentTime = date.getHours();
+if(currentTime < 12) {
+    greeting = "Good morning";
+}
+else if(currentTime > 12 && currentTime < 16) {
+    greeting = "Good afternoon";
+}
+else if(currentTime > 16) {
+    greeting = "Good evening";
+}
+
+document.getElementById("greeting").innerText = greeting + ", " + userName + "!";    
+
 let nextTaskId = 1;
 const list = document.getElementById("tasks-container");
 let tasks = [];
 function addHabit(habit, id) {
-    const text = `<div class="task-holder">
+    const text = `<div class="row">
+    <div class="task-holder">
         <p class="text">${habit}
         </p>
         <button onclick="removeHabit(this)" type="button" class="deletebtn"><i class="fa fa-trash-alt" job="delete"></i></button>
     </div>
     <div class="checkbox-holder">
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+</div>
 </div>`
     const position = "beforeEnd"
     list.insertAdjacentHTML(position, text);
@@ -23,8 +47,6 @@ function addHabit(habit, id) {
         id: id
     })
 }
-
-
 
 const input = document.getElementById("input-habit");
 document.addEventListener("keyup", function (e) {
@@ -43,7 +65,7 @@ function addNewHabit() {
 }
 
 function removeHabit(element) {
-    element.parentNode.nextElementSibling.remove();
+    element.parentNode.parentNode.remove();
     element.parentNode.remove();
 }
 
@@ -51,4 +73,10 @@ function clearTextBox() {
     document.getElementById("input-habit").value = "";
 }
 
-
+function changeColor(color) {
+    document.getElementById("header").style.backgroundColor = color;
+    const taskHolder = document.getElementsByClassName("task-holder");
+        for(var i = 0; i < 100; i++){
+            taskHolder[i].style.backgroundColor = color;
+        }
+}
