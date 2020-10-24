d9239516d0af093b58c8615e0531dbccd97b5d65
hamiat <70755521+hamiat@users.noreply.github.com>
2020-10-24 14:06:32 +0200

    updated all the file & added checked box
    Update the html & css & JS to match recent commits of the main branch.
    Added checked box - they almost work as I want them to.

--- tracker.js
+++ tracker.js
@@ -1,23 +1,48 @@
+const userName = prompt("Welcome! Please enter your name:");
+
+const date = new Date();
+const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+const timeOptions = { hour: '2-digit', minute:'2-digit' };
+document.getElementById("date").innerText = date.toLocaleDateString('en-US', dateOptions);
+document.getElementById("time").innerText = date.toLocaleTimeString('en-US', timeOptions);
+
+let greeting;
+let currentTime = date.getHours();
+if(currentTime < 12) {
+    greeting = "Good morning";
+}
+else if(currentTime > 12 && currentTime < 16) {
+    greeting = "Good afternoon";
+}
+else if(currentTime > 16) {
+    greeting = "Good evening";
+}
+
+document.getElementById("greeting").innerText = greeting + ", " + userName + "!";    
+
 let nextTaskId = 1;
 const list = document.getElementById("tasks-container");
+let tasks = [];
 function addHabit(habit, id) {
-    const text = `<div class="task-holder">
+    const text = `<div class="row">
+    <div class="task-holder">
         <p class="text">${habit}
-        <i class="fa fa-trash-alt toright" job="delete" id="${id}"></i>
         </p>
+        <button onclick="removeHabit(this)" type="button" class="deletebtn"><i class="fa fa-trash-alt" job="delete"></i></button>
     </div>
     <div class="checkbox-holder">
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
-    <div class="task-checkbox"><i class="far fa-square center"></i></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+    <div class="task-checkbox"></div>
+</div>
 </div>`
     const position = "beforeEnd"
     list.insertAdjacentHTML(position, text);
-    container.push({
+    tasks.push({
         name: habit,
         id: id
     })
@@ -28,51 +53,55 @@ document.addEventListener("keyup", function (e) {
     if (e.keyCode == 13) {
         addNewHabit();
     }
-
 })
+
+//checking (all) boxes (hence the loop)
+let checkBox = document.getElementsByClassName('task-checkbox');
+let checkBoxes = checkBox.length;
+for (var i = 0; i < checkBoxes; i++) {
+    checkBox[i].addEventListener('click', completed, false);
+}
+//function that adds CSS styling class (task-completed, created in CSS sheet)
+function completed() {
+    this.classList.toggle("task-completed");
+}
 
 function addNewHabit() {
     const habit = input.value;
     if (habit) {
         addHabit(habit, nextTaskId++);
+        clearTextBox();
     }
     input.value = "";
+
+    //user able to checkboxes for each new habit as well, hence inside the new habit function
+    let checkBox = document.getElementsByClassName('task-checkbox');
+    let checkBoxes = checkBox.length;
+    for (var i = 0; i < checkBoxes; i++) {
+    checkBox[i].addEventListener('click', completed, false);
+    }
+    function completed() {
+    this.classList.toggle("task-completed");
+    }
 }
 
-//save to localStorage
-function ToDos() {
-    let habit;
-    if (localStorage.setItem('habit') === null) {
-        habit = [];
-    } 
-    else {
-        habit = JSON.parse(localStorage.setItem('habit'));
-    }
-
-    habit.push(addHabit);
-    localStorage.setItem('habit', JSON.stringify(habit));
+function removeHabit(element) {
+    element.parentNode.parentNode.remove();
+    element.parentNode.remove();
 }
 
-// checked box
- let checkedBox = document.getElementById('checked-box').addEventListener('click', completed);
+function clearTextBox() {
+    document.getElementById("input-habit").value = "";
+}
 
- function completed() {
-    let checkedBox = document.getElementById('checked-box');
-    checkedBox.classList.toggle("task-checked");  
-}   
-/* 
+function changeColor(color) {
+    document.getElementById("header").style.backgroundColor = color;
+    const taskHolder = document.getElementsByClassName("task-holder");
+        for(var i = 0; i < taskHolder.length; i++){
+            taskHolder[i].style.backgroundColor = color;
+        }
+}
 
-  let checkedBox = document.querySelector('.checkbox-holder');
-checkedBox.addEventListener('click', completed, false);
 
-function completed(e) {
-    //if the element clicked is not the parent element itself
-    if (e.target !== e.currentTarget){
-    // get the ID of the clicked element, filtering out any clicks from the parent
-    let thisItem = e.target.id; 
-    this.classList.toggle("task-checked");
-    
-    }
 
-} 
-*/ 
+
