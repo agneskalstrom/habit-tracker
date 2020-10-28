e46ee455e67b392a918a9335c1c84bb1890edff0
hamiat <70755521+hamiat@users.noreply.github.com>
2020-10-28 15:20:46 +0100

    Toggle fixed, added localStorage & pointer cursors
    Toggle loop moved and put inside the function that displays the tasks (instead of the one that  adds habits.
    Added set to and get items from LocalStorage. As well the prompted username. 
    Added pointer cursors to CSS

--- tracker.js
+++ tracker.js
@@ -1,4 +1,13 @@
-const userName = prompt("Welcome! Please enter your name:");
+// set to & get username from localStorage upon first entry
+window.onload = function greet(){
+    name = localStorage.getItem("name");
+    if (name == null || name == "null"){
+      name = prompt("Welcome! Please enter your name:");
+      localStorage.setItem("name", name);
+    } 
+    document.getElementById("greeting").innerText = greeting + ", " + name + "!";  
+} 
+  
 
 const date = new Date();
 const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
@@ -11,32 +20,24 @@ let currentTime = date.getHours();
 if(currentTime < 12) {
     greeting = "Good morning";
 }
-else if(currentTime >= 12 && currentTime < 16) {
+else if(currentTime > 12 && currentTime < 16) {
     greeting = "Good afternoon";
 }
 else if(currentTime > 16) {
     greeting = "Good evening";
 }
 
-document.getElementById("greeting").innerText = greeting + ", " + userName + "!";    
-
-let color;
-
-function changeColor(newColor) {
-    document.getElementById("header").style.backgroundColor = newColor;
-    const taskHolder = document.getElementsByClassName("task-holder");
-        for(let i = 0; i < taskHolder.length; i++){
-            taskHolder[i].style.backgroundColor = newColor;
-        }   
-    color = newColor;
-}
 
 let nextTaskId = 1;
 const list = document.getElementById("tasks-container");
-let tasks = [];
-function addHabit(habit, id) {
+let tasks = [{
+    task: "Go for a walk",
+    id: 0
+}];
+
+function displayTask(habit, id) {
     const text = `<div class="row">
-    <div class="task-holder" style="background-color:` + color +`;">
+    <div class="task-holder">
         <p class="text">${habit}
         </p>
         <button onclick="removeHabit(this)" type="button" class="deletebtn"><i class="fa fa-trash-alt" job="delete"></i></button>
@@ -49,15 +50,34 @@ function addHabit(habit, id) {
     <div class="task-checkbox"></div>
     <div class="task-checkbox"></div>
     <div class="task-checkbox"></div>
-</div>
-</div>`
+    </div>
+    </div>`
+
     const position = "beforeEnd"
     list.insertAdjacentHTML(position, text);
-    tasks.push({
-        name: habit,
-        id: id
-    })
+
+     //user able to checkboxes for each new habit as well, hence inside the new habit function
+     checkBox = document.getElementsByClassName('task-checkbox');
+     checkBoxes = checkBox.length;
+     for (let i = 0; i < checkBoxes; i++) {
+     checkBox[i].addEventListener('click', completed, false);
+     }
 }
+//save tasks to localStorage
+let saveToLocalStorage = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function addHabit(habit, id) {
+    const task = {
+        task: habit,
+        id: id
+    };
+    displayTask(habit, id);
+    tasks.push(task);  
+    saveToLocalStorage(); 
+}
+
 
 const input = document.getElementById("input-habit");
 document.addEventListener("keyup", function (e) {
@@ -66,13 +86,16 @@ document.addEventListener("keyup", function (e) {
     }
 })
 
+
 function addNewHabit() {
     const habit = input.value;
     if (habit) {
         addHabit(habit, nextTaskId++);
         clearTextBox();
+
     }
     input.value = "";
+
 }
 
 function removeHabit(element) {
@@ -83,3 +106,35 @@ function removeHabit(element) {
 function clearTextBox() {
     document.getElementById("input-habit").value = "";
 }
+
+function changeColor(color) {
+    document.getElementById("header").style.backgroundColor = color;
+    const taskHolder = document.getElementsByClassName("task-holder");
+        for(var i = 0; i < taskHolder.length; i++){
+            taskHolder[i].style.backgroundColor = color;
+        }
+}
+
+//function that adds CSS styling class (task-completed, created in CSS sheet)
+function completed(e) {
+    e.target.classList.toggle("task-completed");
+}
+
+
+
+//get from LocalStorage
+window.addEventListener("load", function () {
+    let getFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+    console.log(getFromLocalStorage);
+    Object.assign(tasks, getFromLocalStorage); //copy from source to target
+    tasks.forEach(item => {
+        displayTask(item.task, item.id);
+    })
+})
+
+
+
+
+
+
+
